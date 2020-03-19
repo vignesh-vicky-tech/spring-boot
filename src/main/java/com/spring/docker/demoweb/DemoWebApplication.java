23f9ff00b379e4486a942704cb7910225ed2d6e7
MarquiseG <meriem.karoun@gmail.com>
2020-03-19 19:12:58 +0100

    Initial commit

---
+++ src/main/java/com/spring/docker/demoweb/DemoWebApplication.java
@@ -0,0 +1,27 @@
+package com.spring.docker.demoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class DemoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public Map<String, Object>test()
+    {
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", "From docker");
+        data.put("coubter", 566);
+        return data;
+    }
+}
